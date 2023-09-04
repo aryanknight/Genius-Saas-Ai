279a5d5737a2dfd26a2985f3b7ffdd3b6869f90f
aryanknight <aryanknight12@gmail.com>
2023-09-05 01:30:34 +0530

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import {UserButton} from '@clerk/nextjs';
+import Image from 'next/image'
+
+export default function DashboardPage() {
+  return (
+    <div>
+        <p>Dashboard Page</p>
+        {/* <UserButton afterSignOutUrl='/'/> */}
+    </div>
+  )
+}
---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./sidebar";
+
+export default function MobileSidebar() {
+  const [isMounted,setIsMounted]=useState(false);
+  useEffect(()=>{
+    setIsMounted(true);
+  },[])
+  if(!isMounted){
+    return null;
+  }
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant={"ghost"} size={"icon"} className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+}
